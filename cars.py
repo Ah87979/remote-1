fda74a81cd26258358d50f6a8fa319176cd58eea
Ahmed <ah87979@gmail.com>
2024-01-31 12:18:51 +0300

    Commit

--- cars.py
+++ cars.py
@@ -3,7 +3,7 @@ import random
 cars = ['BMW', 'Mini', 'Porsche']
 
 def generateModelYear():
-	retrun randon.randRange(1970, 2022)
+	retrun random.randRange(1970, 2022)
 
 print(random.choice(cars))
 print(generateModelYear())
